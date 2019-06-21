06b236eec30a1656bc3abd823db13a7e09f635cf
Chris Martin <chris.bizunit34@gmail.com>
2019-06-21 10:34:46 -0400

    completed wireframes

---
+++ index.js
@@ -0,0 +1,22 @@
+import $ from 'jquery';
+import 'normalize.css';
+
+import './index.css';
+
+import download from './images/download.jpg';
+import LinkedIn from './images/LinkedIn.jpg';
+import makeMoney from './images/makeMoney.jpg';
+
+let downloadImg = document.getElementById('download');
+let LinkedInImg = document.getElementById('LinkedIn');
+let makeMoneyImg = document.getElementById('makeMoney');
+
+downloadImg.src = download;
+LinkedInImg.src = LinkedIn;
+makeMoneyImg.src = makeMoney;
+
+function main() {
+  
+}
+
+$(main);
