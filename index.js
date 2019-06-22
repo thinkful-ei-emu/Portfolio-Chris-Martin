77670308559521af6897e6a806fb0446d0e31b12
Chris Martin <chris.bizunit34@gmail.com>
2019-06-22 15:54:29 -0400

    added css and logo

--- index.js
+++ index.js
@@ -1,31 +1,54 @@
-/* global store, $, api*/
+/* global $*/
 
-function generateAboutMe() {
-
+function removeHome() {
+  $('.js-headline').addClass('hidden');
+  $('.js-projects').addClass('hidden');
+  $('.js-about-me').addClass('hidden');
+  $('.js-navigation-list').removeClass('hidden');
+  $('.page-footer').removeClass('hidden');
 }
 
-function generateProjects() {
-
+function addHome() {
+  $('.js-headline').removeClass('hidden');
+  $('.js-projects').removeClass('hidden');
+  $('.js-about-me').removeClass('hidden');
+  $('.js-navigation-list').addClass('hidden');
+  $('.page-footer').addClass('hidden');
 }
 
 function handleAboutMeClick() {
   $('.js-about-me').click(event => {
     event.preventDefault();
-    $('#js-submit-bookmark-form').removeClass('hidden');
-    $('.js-projects').addClass('hidden');
+    $('.projects-section').addClass('hidden');    
+    $('.about-me-section').removeClass('hidden');
+    removeHome();
   });
 }
 
 function handleProjectsClick() {
   $('.js-projects').click(event => {
     event.preventDefault();
-    $('#js-submit-bookmark-form').removeClass('hidden');
-    $('.js-about-me').addClass('hidden');
+    $('.about-me-section').addClass('hidden');
+    $('.projects-section').removeClass('hidden');
+    removeHome();
+  });
+}
+
+function handleHomeClick() {
+  $('.js-go-home').click(event => {
+    event.preventDefault();
+    $('.projects-section').addClass('hidden');
+    $('.about-me-section').addClass('hidden');
+    addHome();
   });
 }
 
 function main() {
-  
+  handleAboutMeClick();
+  handleProjectsClick();
+  removeHome();
+  addHome();
+  handleHomeClick();
 }
 
 $(main);
